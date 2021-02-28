9d71e2e3a51870274378183b0022e269c42a3030
Mari <shchepetkova.maria1995@gmail.com>
2021-02-28 22:53:17 +0300

    Tasks 1.2.3 and 1.2.4 has been added, task 1.2 all done.

--- Task 1/Task 1.2/Task 1.2/Program.cs
+++ Task 1/Task 1.2/Task 1.2/Program.cs
@@ -21,6 +21,22 @@ namespace Task_1._2
 
             DividingLine();
             Console.ReadKey();
+
+            //Task 1.2.3
+            Console.WriteLine("Task 1.2.3");
+            Console.WriteLine();
+            LowerCase();
+
+            DividingLine();
+            Console.ReadKey();
+
+            //Task 1.2.4
+            Console.WriteLine("Task 1.2.4");
+            Console.WriteLine();
+            Validator();
+
+            DividingLine();
+            Console.ReadKey();
         }
 
         static void Averages()
@@ -51,7 +67,9 @@ namespace Task_1._2
             }
 
             //результат округляется до целого в меньшую сторону
-            int average = lengthWords / sumWords;
+            int average = 0;
+            if (lengthWords != 0 || sumWords != 0)
+                average = lengthWords / sumWords;
             Console.WriteLine();
             Console.WriteLine("Средняя длина слова во введённой текстовой строке: {0}.", average);
         }
@@ -75,6 +93,80 @@ namespace Task_1._2
             }
             Console.WriteLine();
             Console.WriteLine("Результат: " + result);
+        }
+
+        static void LowerCase()
+        {
+
+            Console.Write("(Вариант без *) Введите строку: ");
+            string str1 = Console.ReadLine();
+            string[] wordsArray1 = str1.Split(" ");
+            int sumIsLowerWords1 = 0;
+            foreach (string word in wordsArray1)
+            {
+                if (!word.Equals(""))
+                {
+                    if (Char.IsLower(word, 0))
+                    {
+                        sumIsLowerWords1++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество слов, начинающихся с маленькой буквы: {0}", sumIsLowerWords1);
+
+            Console.WriteLine();
+            Console.Write("(Вариант со *) Введите строку: ");
+            string str2 = Console.ReadLine();
+            string[] wordsArray2 = str2.Split(' ', ';', ',', ':');
+            int sumIsLowerWords2 = 0;
+            foreach (string word in wordsArray2)
+            {
+                if (!word.Equals(""))
+                {
+                    if (Char.IsLower(word, 0))
+                    {
+                        sumIsLowerWords2++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество слов, начинающихся с маленькой буквы: {0}", sumIsLowerWords2);
+        }
+
+        static void Validator()
+        {
+            char[] dividing = new char[3] { '.', '!', '?' };
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            string[] wordsArray = str.Split(" ");
+            string result = "";
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    wordsArray[i] = Char.ToUpper(wordsArray[0][0]) + "" 
+                        + wordsArray[0].Substring(1, wordsArray[0].Length - 1);
+                }
+                for (int j = 0; j < dividing.Length; j++)
+                {
+                    if (wordsArray[i][wordsArray[i].Length - 1].Equals(dividing[j]))
+                    {
+                        if (i + 1 < wordsArray.Length)
+                        {
+                            wordsArray[i + 1] = Char.ToUpper(wordsArray[i + 1][0]) + ""
+                                + wordsArray[i + 1].Substring(1, wordsArray[i + 1].Length - 1);
+                        }
+                    }
+                }
+                if (i != 0)
+                {
+                    result += " ";
+                }
+                result += wordsArray[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine(result);
         }
 
         static void DividingLine()
