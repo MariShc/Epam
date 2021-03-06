62df48b29649c1d68a8de73ffe0be5c34045a895
Mari <shchepetkova.maria1995@gmail.com>
2021-03-06 20:59:01 +0300

    Tasks 2.1.1 has been added.

---
+++ Task 2/Task 2.1/Task 2.1.1/Task 2.1.1/MyOwnString.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Хрень
+{
+    class MyOwnString
+    {
+
+        private char[] myString { get; set; }
+
+        public MyOwnString()
+        {
+            myString = null;
+        }
+
+        public MyOwnString(char[] charArray)
+        {
+            myString = new char[charArray.Length];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                myString[i] = charArray[i];
+            }
+        }
+
+        public bool MyEquals(MyOwnString arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.myString.Length; i++)
+            {
+                if (myString.Length == arr.myString.Length)
+                {
+                    if (arr.myString[i] == myString[i])
+                        counter++;
+                }
+            }
+            return (counter == myString.Length);
+        }
+
+        public int MyLength()
+        {
+            return this.myString.Length;
+        }
+
+        public char[] ToCharArray()
+        {
+            return myString;
+        }
+
+        public MyOwnString FromCharArray(char[] array)
+        {
+            myString = array;
+
+            return this;
+        }
+
+        public MyOwnString MyConcat(char[] array)
+        {
+            char[] newArray = new char[myString.Length + array.Length];
+            for (int i = 0; i < myString.Length; i++)
+            {
+                newArray[i] = myString[i];
+            }
+            int k = 0;
+            for (int j = myString.Length; j < myString.Length + array.Length; j++)
+            {
+                if (k < array.Length)
+                {
+                    newArray[j] = array[k];
+                    k++;
+                }
+            }
+            myString = newArray;
+            return this;
+        }
+
+        public int MyIndexOf(char c)
+        {
+            for (int i = 0; i < myString.Length; i++)
+            {
+                if (myString[i] == c)
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool MyContains(char c) => MyIndexOf(c) >= 0;
+
+        //Конкатенация двух строк в третью
+        public MyOwnString MyConcat2(char[] array1, char[] array2)
+        {
+            myString = MyConcat(array1).MyConcat(array2).myString;
+            return this;
+        }
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.1/Task 2.1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Хрень
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] a = "Привет".ToCharArray();
+            MyOwnString str = new MyOwnString(a);
+
+            MyOwnString str1 = new MyOwnString(ReadLine(1));
+            MyOwnString str2 = new MyOwnString(ReadLine(2));
+
+            Console.WriteLine("Сравнение строк 1 и 2: {0}", str1.MyEquals(str2));
+            Console.WriteLine($"Длина строки 1 равна {str1.MyLength()}");
+
+            str1.MyConcat2(ReadLine(3), ReadLine(4));
+            Console.WriteLine(str1.ToCharArray());
+
+            Console.WriteLine($"В строке 1 есть символ 'м': { str1.MyContains('м')}");
+            Console.WriteLine(str1.MyIndexOf('а'));
+        }
+
+        private static char[] ReadLine(int numberLine)
+        {
+            Console.WriteLine("Введите строку {0}:", numberLine);
+            return Console.ReadLine().ToCharArray();
+        }
+    }
+}
