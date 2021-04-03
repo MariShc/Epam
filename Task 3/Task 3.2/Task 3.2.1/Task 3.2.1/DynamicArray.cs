fdf2ea84f210e207312949902af6968b7d52073b
Mari <shchepetkova.maria1995@gmail.com>
2021-04-03 17:54:10 +0300

    Task 3.2.1 has been added.

---
+++ Task 3/Task 3.2/Task 3.2.1/Task 3.2.1/DynamicArray.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_3._2._1
+{
+    class DynamicArray<T> : IEnumerable, IEnumerable<T>
+    {
+        private const int defaultCapacity = 8;
+        private T[] MyArray { get; set; }
+
+        private int length;
+
+        private int capacity;
+        public int Length
+        {
+            get => length;
+        }
+
+        public int Capacity
+        {
+            get => capacity;
+
+            set
+            {
+                if (value < Length)
+                {
+                    throw new ArgumentOutOfRangeException("value", value, "ArgumentOutOfRange_SmallCapacity");
+                }
+                T[] arrayNew = new T[value];
+                if (length > 0)
+                {
+                    Array.Copy(MyArray, 0, arrayNew, 0, length);
+                }
+                MyArray = arrayNew;
+                capacity = value;
+
+
+            }
+        }
+
+        public DynamicArray()
+        {
+            Capacity = defaultCapacity;
+            MyArray = new T[Capacity];
+        }
+
+        public DynamicArray(int _capacity)
+        {
+            if (_capacity < 0)
+                throw new ArgumentOutOfRangeException("capacity", _capacity, "ArgumentOutOfRange_NeedNonNegNum");
+            else
+            {
+                Capacity = _capacity;
+                MyArray = new T[Capacity];
+            }
+        }
+
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            else
+                MyArray = collection.ToArray();
+
+            length = collection.ToArray().Length;
+            Capacity = length * 2;
+        }
+
+        public void Add(T item)
+        {
+            if (length == Capacity) Capacity *= 2;
+            MyArray[length++] = item;
+        }
+
+        public void AddRange (IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            int countCollection = collection.Count();
+            while (Capacity < length + countCollection) Capacity *= 2;
+            length = length + countCollection;
+            Array.Copy(collection.ToArray(), 0, MyArray, length, countCollection);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int shiftIndex = index + 1;
+            if (shiftIndex < length)
+            {
+                Array.Copy(MyArray, shiftIndex, MyArray, index, length - shiftIndex);
+            }
+            length--;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (MyArray[i].Equals(item))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index >= length)
+            {
+                throw new ArgumentOutOfRangeException("index", index, "ArgumentOutOfRange");
+            }
+
+            if (MyArray.Length == length) Capacity *= 2;
+
+            if (index < length)
+                Array.Copy(MyArray, index, MyArray, index + 1, length - index);
+
+            MyArray[index] = item;
+            length++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < length; i++)
+                yield return MyArray[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= length)
+                    throw new ArgumentOutOfRangeException("index", index, "ArgumentOutOfRange");
+                return MyArray[index];
+            }
+
+            set
+            {
+                if (index >= length)
+                    throw new ArgumentOutOfRangeException("index", index, "ArgumentOutOfRange");
+                MyArray[index] = value;
+            }
+        }
+    }
+}
---
+++ Task 3/Task 3.2/Task 3.2.1/Task 3.2.1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckAddAndAddRange();
+            CheckRemoveAndInsert();
+            CheckMetodsIEnumerableIenumerator();
+            CheckIndexator();
+        }
+
+        static void CheckAddAndAddRange()
+        {
+            List<string> myList = new List<string> { "ff", "hhh", "fgg", "hhh", "fgg", "d", "ff", "j" };
+            DynamicArray<string> arrayString = new DynamicArray<string>(myList);
+            DynamicArray<string> dynamicArray = new DynamicArray<string>();
+            dynamicArray.Add("Maria");
+            dynamicArray.Add("Oksana");
+            dynamicArray.Add("Maksim");
+            dynamicArray.Add("Svetlana");
+            dynamicArray.Add("Tatyana");
+            dynamicArray.Add("Igor");
+            dynamicArray.Add("Ekaterina");
+            dynamicArray.Add("Dmitrii");
+            dynamicArray.Add("Olga");
+            dynamicArray.Add("Inna");
+            Console.WriteLine("Проверка методов Add и AddRange, а также свойств Capacity и Length:");
+            Console.WriteLine(dynamicArray.Length);
+            Console.WriteLine(dynamicArray.Capacity);
+            Console.WriteLine(arrayString.Length);
+            Console.WriteLine(arrayString.Capacity);
+            dynamicArray.AddRange(myList);
+            Console.WriteLine("Capacity нового массива: " + dynamicArray.Capacity);
+            Console.WriteLine("Length нового массива: " + dynamicArray.Length);
+            Console.WriteLine();
+        }
+
+        static void CheckRemoveAndInsert()
+        {
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+            dynamicArray.Add(9);
+            dynamicArray.Add(45);
+            dynamicArray.Add(0);
+
+            dynamicArray.Add(0);
+            dynamicArray.Add(0);
+            dynamicArray.Add(0);
+            dynamicArray.Add(0);
+            dynamicArray.Add(0);
+
+            Console.WriteLine("Проверка методов Remove и Insert:");
+            Console.WriteLine("До удаления элемента, Length: " + dynamicArray.Length);
+            Console.WriteLine("До удаления эелемента, Capacity: " + dynamicArray.Capacity);
+            dynamicArray.Remove(45);
+            Console.WriteLine("После удаления элемента, Length: " + dynamicArray.Length);
+            Console.WriteLine("После удаления эелемента, Capacity: " + dynamicArray.Capacity);
+            dynamicArray.Insert(1, 999);
+            Console.WriteLine("После вставки элемента, Length: " + dynamicArray.Length);
+            Console.WriteLine("После вставки эелемента, Capacity: " + dynamicArray.Capacity);
+            Console.WriteLine();
+        }
+
+        static void CheckMetodsIEnumerableIenumerator()
+        {
+            Console.WriteLine("Проверка методов, реализующих интерфейсы IEnumerable и IEnumerator:");
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            foreach (int item in dynamicArray)
+                Console.WriteLine(item);
+            Console.WriteLine();
+        }
+        static void CheckIndexator()
+        {
+            Console.WriteLine("Проверка работы индексатора:");
+            DynamicArray<string> dynamicArray = new DynamicArray<string>();
+            dynamicArray.Add("kkk");
+            dynamicArray.Add("hhh");
+            dynamicArray.Add("ppp");
+            for (int i = 0; i < dynamicArray.Length; i++)
+                Console.WriteLine(dynamicArray[i]);
+        }
+    }
+}
