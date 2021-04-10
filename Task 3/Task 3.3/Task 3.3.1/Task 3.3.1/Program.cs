5de924240b2a0a7e9e8b21c80e945f7a915e911e
Mari <shchepetkova.maria1995@gmail.com>
2021-04-10 12:23:55 +0300

    Task 3.3.1 has been added

---
+++ Task 3/Task 3.3/Task 3.3.1/Task 3.3.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task_3._3._1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int[] mas = new int[] { 2, 6, 7 };
+            Console.WriteLine("Возведение каждого элемента массива в степень 2:");
+            mas.SomeAction(Exp);
+            Console.WriteLine("Умножение каждого элемента массива на 3:");
+            mas.SomeAction(MultBy3);
+            Console.WriteLine("Некоторая магическая сумма:");
+            mas.SomeAction(SomeMagicSum);
+            Console.WriteLine("Сумма элементов массива:");
+            mas.SumItems();
+            Console.WriteLine("Среднее значение в массиве:");
+            mas.Average();
+            int[] mas2 = new int[] { 1, 2, 2, 4, 2, 5, 4, 4, 5 };
+            Console.WriteLine("Самый часто повторяемый элемент в массиве: [" + string.Join(",", mas2) + "]");
+            mas2.MostFrequent();
+        }
+
+        public static int Exp(int elem) => (int)Math.Pow(elem, 2);
+
+        public static float Exp(float elem) => (float)Math.Pow(elem, 2);
+
+        public static double Exp(double elem) => (double)Math.Pow(elem, 2);
+
+        public static byte Exp(byte elem) => (byte)Math.Pow(elem, 2);
+
+        public static short Exp(short elem) => (short)Math.Pow(elem, 2);
+
+        public static int MultBy3(int elem) => elem * 3;
+
+        public static float MultBy3(float elem) => elem * 3;
+
+        public static double MultBy3(double elem) => elem * 3;
+
+        public static byte MultBy3(byte elem) => (byte)(elem * 3);
+
+        public static short MultBy3(short elem) => (short)(elem * 3);
+
+        public static int SomeMagicSum(int elem) => elem + 10000;
+
+        public static float SomeMagicSum(float elem) => elem + 10000;
+
+        public static double SomeMagicSum(double elem) => elem + 10000;
+
+        public static byte SomeMagicSum(byte elem) => (byte)(elem + 10000);
+
+        public static short SomeMagicSum(short elem) => (short)(elem + 10000);
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.1/Task 3.3.1/SuperArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_3._3._1
+{
+    public static class SuperArray
+    {
+        public static void SomeAction<T>(this T[] mas, Func<T, T> action)
+        {
+            if (action != null)
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] = action(mas[i]);
+                    Console.WriteLine(mas[i]);
+                }
+            }
+        }
+        public static void SumItems(this int[] array) => Console.WriteLine(array.Sum());
+
+        public static void SumItems(this float[] array) => Console.WriteLine(array.Sum());
+
+        public static void SumItems(this double[] array) => Console.WriteLine(array.Sum());
+
+        public static void SumItems(this byte[] array)
+        {
+            byte sumItems = 0;
+
+            foreach (byte item in array)
+                sumItems += item;
+
+            Console.WriteLine(sumItems);
+        }
+
+        public static void SumItems(this short[] array)
+        {
+            short sumItems = 0;
+
+            foreach (short item in array)
+                sumItems += item;
+
+            Console.WriteLine(sumItems);
+        }
+
+        public static void Average(this int[] array) => Console.WriteLine(array.Sum() / array.Length);
+
+        public static void Average(this float[] array) => Console.WriteLine(array.Sum() / array.Length);
+
+        public static void Average(this double[] array) => Console.WriteLine(array.Sum() / array.Length);
+
+        public static void Average(this byte[] array)
+        {
+            byte sumItems = 0;
+
+            foreach (byte item in array)
+                sumItems += item;
+
+            byte average = (byte)(sumItems / array.Length);
+            Console.WriteLine(average);
+        }
+
+        public static void Average(this short[] array)
+        {
+            short sumItems = 0;
+
+            foreach (short item in array)
+                sumItems += item;
+
+            short average = (short)(sumItems / array.Length);
+            Console.WriteLine(average);
+        }
+
+        public static void MostFrequent(this int[] array) => 
+            Console.WriteLine(array.GroupBy(i => i).OrderByDescending(i => i.Count()).ToArray()[0].Key);
+
+        public static void MostFrequent(this float[] array) =>
+            Console.WriteLine(array.GroupBy(i => i).OrderByDescending(i => i.Count()).ToArray()[0].Key);
+
+        public static void MostFrequent(this double[] array) =>
+            Console.WriteLine(array.GroupBy(i => i).OrderByDescending(i => i.Count()).ToArray()[0].Key);
+
+        public static void MostFrequent(this byte[] array) =>
+            Console.WriteLine(array.GroupBy(i => i).OrderByDescending(i => i.Count()).ToArray()[0].Key);
+
+        public static void MostFrequent(this short[] array) =>
+            Console.WriteLine(array.GroupBy(i => i).OrderByDescending(i => i.Count()).ToArray()[0].Key);
+    }
+}
