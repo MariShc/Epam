03a7d50553af9e78d0c743456bcc65a4fd1ef723
Mari <shchepetkova.maria1995@gmail.com>
2021-04-11 13:14:30 +0300

    Task 3.3.2 has been added

---
+++ Task 3/Task 3.3/Task 3.3.2/Task 3.3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_3._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            Console.WriteLine(str);
+            Console.WriteLine(str.DefineLanguage());
+            string str2 = "Маша";
+            Console.WriteLine(str2);
+            Console.WriteLine(str2.DefineLanguage());
+            string str3 = "1234";
+            Console.WriteLine(str3);
+            Console.WriteLine(str3.DefineLanguage());
+            string str4 = "Tam тадам 123";
+            Console.WriteLine(str4);
+            Console.WriteLine(str4.DefineLanguage());
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.2/Task 3.3.2/SuperString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._3._2
+{
+    public static class SuperString
+    {
+        public static string DefineLanguage(this string str)
+        {
+            int rusCount = 0;
+            int engCount = 0;
+            int numCount = 0;
+            int mixCount = 0;
+            string language = "";
+            foreach (char c in str)
+            {
+                if ((c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я'))
+                    rusCount++;
+                else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                    engCount++;
+                else if (c >= '0' && c <= '9')
+                    numCount++;
+                else
+                    mixCount++;
+            }
+            if (str.Length == rusCount) language = "Russian";
+            else if (str.Length == engCount) language = "English";
+            else if (str.Length == numCount) language = "Number";
+            else language = "Mixed";
+            return language;
+        }
+    }
+}
